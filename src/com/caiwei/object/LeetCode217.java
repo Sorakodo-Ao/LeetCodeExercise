f660469b37497ea508f6cabf32f2177699094974
Sorako <2818688939@qq.com>
2024-08-28 15:32:23 +0800

    斐波那契数列

--- src/com/caiwei/object/LeetCode217.java
+++ src/com/caiwei/object/LeetCode217.java
@@ -15,6 +15,9 @@ class LeetCode217 {
         System.out.println(b);
     }
 
+   /* 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；
+   如果数组中每个元素互不相同，返回 false 。*/
+
     public boolean containsDuplicate(int[] nums) {
 
 /*        HashSet<Integer> set = new HashSet<>();
--- src/com/caiwei/object/Test.java
+++ src/com/caiwei/object/Test.java
@@ -1,11 +1,12 @@
 package com.caiwei.object;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        /*List<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
         list.add("3");
@@ -13,6 +14,8 @@ public class Test {
         System.out.println(list.get(0));
 
         System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.min(2,6));
+        System.out.println(Integer.min(2,6));*/
+        Boolean[] booleans = new Boolean[10];
+        System.out.println(Arrays.toString(booleans));
     }
 }
