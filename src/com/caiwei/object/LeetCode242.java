48fc48a12316f069116f51493a0bba24970230ed
Sorako <2818688939@qq.com>
2024-08-29 14:37:31 +0800

    有效的字母异位词

---
+++ src/com/caiwei/object/LeetCode242.java
@@ -0,0 +1,100 @@
+package com.caiwei.object;
+
+import org.junit.jupiter.api.Test;
+
+//有效的字母异位词
+public class LeetCode242 {
+    @Test
+    void test() {
+        //boolean anagram = isAnagram("anagram", "nagaram");
+        boolean anagram = isAnagram("rac", "cat");
+        System.out.println(anagram);
+    }
+
+    /*  给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。s 和 t 仅包含小写字母
+        字母异位词 是通过重新排列不同单词或短语的字母而形成的单词或短语，通常只使用所有原始字母一次。
+        示例 1:
+        输入: s = "anagram", t = "nagaram"
+        输出: true
+        示例 2:
+        输入: s = "rat", t = "car"
+        输出: false
+    */
+    /*暴力哈希map匹配
+    public boolean isAnagram(String s, String t) {
+        //字符串为空，false
+        if (s == null || s.equals("")|| t == null || t.equals("")) {
+            return false;
+        }
+        //字符串长度不相等，false
+        if (s.length() != t.length()) {
+            return false;
+        }
+        //两个字符串相等，true;
+        if (s.equals(t)) {
+            return true;
+        }
+        String sParam;
+        String tParam;
+        Map<String, Integer> sMap = new HashMap<>();
+        Map<String, Integer> tMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            sParam = String.valueOf(s.charAt(i));
+            if (sMap.containsKey(sParam)) {
+                sMap.replace(sParam, sMap.get(sParam) + 1);
+            } else {
+                sMap.put(sParam, 1);
+            }
+            tParam = String.valueOf(t.charAt(i));
+            if (tMap.containsKey(tParam)) {
+                tMap.replace(tParam, tMap.get(tParam) + 1);
+            } else {
+                tMap.put(tParam, 1);
+            }
+        }
+        //两个map长度不相等
+        if(sMap.size() != tMap.size()){
+            return false;
+        }
+        //map的key的value 不相同
+        for (String key : sMap.keySet()) {
+            if(!tMap.containsKey(key) || !Objects.equals(tMap.get(key), sMap.get(key))){
+                return false;
+            }
+        }
+        return true;
+    }*/
+    public boolean isAnagram(String s, String t) {
+        //字符串为空，false
+        if (s == null || s.equals("") || t == null || t.equals("")) {
+            return false;
+        }
+        //字符串长度不相等，false
+        if (s.length() != t.length()) {
+            return false;
+        }
+        //两个字符串相等，true;
+        if (s.equals(t)) {
+            return true;
+        }
+        //s 和 t 仅包含小写字母
+        int[] arr = new int['z' + 1];
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i);
+            arr[index]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            int index = t.charAt(i);
+            arr[index]--;
+        }
+
+        boolean flag = true;
+        for (int i : arr) {
+            if(i != 0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
--- src/com/caiwei/object/Test.java
+++ src/com/caiwei/object/Test.java
@@ -1,22 +1,32 @@
 package com.caiwei.object;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        /*List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(5);
 
         System.out.println(list.size());
         System.out.println(list.get(0));
-        /*int indexOfValue = list.indexOf(5);
-        Integer removed = list.remove(indexOfValue);*/
+        *//*int indexOfValue = list.indexOf(5);
+        Integer removed = list.remove(indexOfValue);*//*
         boolean removed = list.remove(Integer.valueOf(5));
         System.out.println(removed);
-        System.out.println(list.toString());
-
+        System.out.println(list.toString());*/
+        /*Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        System.out.println(map.containsKey("a"));
+        map.replace("a", 3);
+        System.out.println(map);
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("a", 3);
+        System.out.println(Objects.equals(map.get("a"), map1.get("a")));*/
+        int[] arr = new int[2];
+        System.out.println(arr[0]);
     }
 }
